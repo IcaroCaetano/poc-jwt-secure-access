741e5a3db38e62937fed2526e2ce024b183f89ea
IcaroCaetano <icaro.caetano@gmail.com>
2025-10-15 19:11:40 -0300

    first commit

---
+++ src/main/java/com/myprojecticaro/poc_jwt_secure_access/PocJwtSecureAccessApplication.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_jwt_secure_access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJwtSecureAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJwtSecureAccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojecticaro/poc_jwt_secure_access/PocJwtSecureAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_jwt_secure_access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocJwtSecureAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

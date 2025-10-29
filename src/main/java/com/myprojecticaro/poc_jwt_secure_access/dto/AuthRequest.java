2dd396d4b61898f732ed979e131cc9b057b0ad30
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-10-29 20:19:19 -0300

    Update AuthRequest.java

--- src/main/java/com/myprojecticaro/poc_jwt_secure_access/dto/AuthRequest.java
+++ src/main/java/com/myprojecticaro/poc_jwt_secure_access/dto/AuthRequest.java
@@ -1,1 +1,27 @@
+package com.myprojecticaro.poc_jwt_secure_access.dto;
+
+/**
+ * DTO used to receive authentication requests containing
+ * username and password.
+ */
+public class AuthRequest {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
 
